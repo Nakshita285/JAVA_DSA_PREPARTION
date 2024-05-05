2381ad6b7d066224bd3b481faa4289b5a3cb160d
Nakshita <nakshita.malhotra.61@gmail.com>
2024-05-05 19:18:44 +0530

    loops introductions

--- src/Section_2_Loops/LoopsIntro.java
+++ src/Section_2_Loops/LoopsIntro.java
@@ -1,7 +1,20 @@
 package Section_2_Loops;
 
+import java.util.Scanner;
+
 public class LoopsIntro {
     public static void main(String[] args) {
-        System.out.println("Starting with loops");
+        Scanner scn = new Scanner(System.in);
+        //Question 1: Print number from 1 to 5
+        for(int i=1; i<=5;i++){
+            System.out.print( i + " " );
+        }
+        System.out.println();
+
+        // Question 2: Print numbers from 1 to n
+        int num = scn.nextInt();
+        for (int i=1; i<=num; i++){
+            System.out.print(i + " ");
+        }
     }
 }
